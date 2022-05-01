808b83a5859a5fee5379d4fc58bdfec5bfff7bfd
andreytutygin <andr3qaa@gmail.com>
2022-05-01 11:49:05 +0300

    Made a section forWhom

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,1 +1,2 @@
 import './components/team__cards-swiper.js';
+import './components/forWhom-slider.js';
---
+++ src/js/components/forWhom-slider.js
@@ -0,0 +1,6 @@
+import Swiper from 'swiper/bundle';
+
+const teamSwiper = new Swiper('.forWhom-slider', {
+    slidesPerView: "auto",
+    spaceBetween: 20,
+});
